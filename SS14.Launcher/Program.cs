d9fd3c1c72390fa4d5ad7ca05b9205b94cee4a2a
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2019-03-31 22:27:47 +0200

    Initial Commit

---
+++ SS14.Launcher/Program.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Contracts;
+using System.Globalization;
+using System.IO;
+using System.IO.Compression;
+using System.Net.Http;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace SS14.Launcher
+{
+    internal static class Program
+    {
+        private const string JenkinsBaseUrl = "https://builds.spacestation14.io/jenkins";
+        private const string JenkinsJobName = "SS14 Content";
+
+        private static HttpClient _httpClient;
+
+        public static async Task Main(string[] args)
+        {
+            _httpClient = new HttpClient();
+
+            var dataDir = GetUserDataDir();
+            // Ensure data dir exists.
+            Directory.CreateDirectory(dataDir);
+
+            await RunUpdate(dataDir);
+
+            LaunchClient();
+        }
+
+        private static void LaunchClient()
+        {
+            var binPath = GetClientBinPath();
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                var process = Process.Start(new ProcessStartInfo
+                {
+                    FileName = "mono",
+                    ArgumentList = { "SS14.Client.exe" },
+                    WorkingDirectory = binPath,
+                    UseShellExecute = false,
+                });
+                // I don't know why but .NET Core closes the child process on exit??
+                process?.WaitForExit();
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                var process = Process.Start(new ProcessStartInfo
+                {
+                    FileName = "SS14.Client.exe",
+                    WorkingDirectory = binPath,
+                });
+                process?.WaitForExit();
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                // TODO: does this cause macOS to make a security warning?
+                // If it does we'll have to manually launch the contents, which is simple enough.
+                var process = Process.Start(new ProcessStartInfo
+                {
+                    FileName = "open",
+                    Arguments = "Space Station 14.app",
+                    WorkingDirectory = binPath,
+                });
+                process?.WaitForExit();
+            }
+            else
+            {
+                throw new NotSupportedException("Unsupported platform.");
+            }
+        }
+
+        private static async Task RunUpdate(string dataDir)
+        {
+            Console.WriteLine("Checking for update...");
+            var jobUri = new Uri($"{JenkinsBaseUrl}/job/{Uri.EscapeUriString(JenkinsJobName)}/api/json");
+            var jobDataResponse = await _httpClient.GetAsync(jobUri);
+            if (!jobDataResponse.IsSuccessStatusCode)
+            {
+                throw new Exception($"Got bad status code {jobDataResponse.StatusCode} from Jenkins.");
+            }
+
+            var jobInfo = JsonConvert.DeserializeObject<JenkinsJobInfo>(
+                await jobDataResponse.Content.ReadAsStringAsync());
+            var latestBuildNumber = jobInfo.LastSuccessfulBuild.Number;
+
+            var versionFile = Path.Combine(dataDir, "current_build");
+            bool needUpdate;
+            if (File.Exists(versionFile))
+            {
+                var buildNumber = int.Parse(File.ReadAllText(versionFile, Encoding.UTF8), CultureInfo.InvariantCulture);
+                needUpdate = buildNumber != latestBuildNumber;
+                if (needUpdate)
+                {
+                    Console.WriteLine("Current version ({0}) is out of date, updating to {1}.", buildNumber,
+                        latestBuildNumber);
+                }
+            }
+            else
+            {
+                Console.WriteLine("As it turns out, we don't have any version yet. Time to update.");
+                // Version file doesn't exist, assume first run or whatever.
+                needUpdate = true;
+            }
+
+            if (!needUpdate)
+            {
+                Console.WriteLine("No update needed!");
+                return;
+            }
+
+            var binPath = GetClientBinPath();
+
+            if (!Directory.Exists(binPath))
+            {
+                Directory.CreateDirectory(binPath);
+            }
+            else
+            {
+                ClearDirectory(binPath);
+            }
+
+            // Download new version and extract it.
+            using (var stream = await _downloadArtifact(latestBuildNumber, GetBuildFilename()))
+            {
+                ExtractZipToDirectory(binPath, stream);
+            }
+
+            // Write version to disk.
+            File.WriteAllText(versionFile, latestBuildNumber.ToString(CultureInfo.InvariantCulture), Encoding.UTF8);
+
+            Console.WriteLine("Update done!");
+        }
+
+        private static async Task<Stream> _downloadArtifact(int buildNumber, string fileName)
+        {
+            var artifactUri
+                = new Uri(
+                    $"{JenkinsBaseUrl}/job/{Uri.EscapeUriString(JenkinsJobName)}/{buildNumber}/artifact/release/{Uri.EscapeUriString(fileName)}");
+
+            var artifactResponse = await _httpClient.GetAsync(artifactUri);
+            if (!artifactResponse.IsSuccessStatusCode)
+            {
+                throw new Exception($"Got bad status code {artifactResponse.StatusCode} from Jenkins.");
+            }
+
+            return await artifactResponse.Content.ReadAsStreamAsync();
+        }
+
+        private static void ExtractZipToDirectory(string directory, Stream zipStream)
+        {
+            using (var zipArchive = new ZipArchive(zipStream))
+            {
+                zipArchive.ExtractToDirectory(directory);
+            }
+        }
+
+        private static void ClearDirectory(string directory)
+        {
+            var dirInfo = new DirectoryInfo(directory);
+            foreach (var fileInfo in dirInfo.EnumerateFiles())
+            {
+                fileInfo.Delete();
+            }
+
+            foreach (var childDirInfo in dirInfo.EnumerateDirectories())
+            {
+                childDirInfo.Delete(true);
+            }
+        }
+
+        [Pure]
+        private static string GetBuildFilename()
+        {
+            string platform;
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                platform = "Windows";
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                platform = "Linux";
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                platform = "macOS";
+            }
+            else
+            {
+                throw new NotSupportedException("Unsupported platform.");
+            }
+
+            return $"SS14.Client_{platform}_x64.zip";
+        }
+
+        [Pure]
+        private static string GetClientBinPath()
+        {
+            return Path.Combine(GetUserDataDir(), "client_bin");
+        }
+
+        [Pure]
+        private static string GetUserDataDir()
+        {
+            string appDataDir;
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                // SpecialFolder.ApplicationData is $XDG_CONFIG_HOME on Linux.
+                // I'd like $XDG_DATA_HOME. SpecialFolder.LocalApplicationData is but eh.
+                // Just do it manually not that hard.
+                var xdgDataHome = Environment.GetEnvironmentVariable("XDG_DATA_HOME");
+                if (xdgDataHome == null)
+                {
+                    var home = Environment.GetEnvironmentVariable("HOME");
+                    if (home == null)
+                    {
+                        throw new Exception("$HOME should always be set on a POSIX system.");
+                    }
+
+                    appDataDir = Path.Combine(home, ".local", "share");
+                }
+                else
+                {
+                    appDataDir = xdgDataHome;
+                }
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                // TODO: Is SpecialFolder.ApplicationData incorrect on .NET Core on macOS?
+                // I know it is on Mono and tries to follow the XDG base directory spec.
+                var home = Environment.GetEnvironmentVariable("HOME");
+                if (home == null)
+                {
+                    throw new Exception("$HOME should always be set on a POSIX system.");
+                }
+
+                appDataDir = Path.Combine(home, "Library", "Application Support");
+            }
+            else
+            {
+                appDataDir = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            }
+
+            return Path.Combine(appDataDir, "Space Station 14", "launcher");
+        }
+    }
+
+#pragma warning disable 649
+    [Serializable]
+    internal class JenkinsJobInfo
+    {
+        public JenkinsBuildRef LastSuccessfulBuild;
+    }
+
+    [Serializable]
+    internal class JenkinsBuildRef
+    {
+        public int Number;
+        public string Url;
+    }
+
+    [Serializable]
+    internal class JenkinsBuildInfo
+    {
+        public List<JenkinsArtifactInfo> Artifacts;
+    }
+
+    [Serializable]
+    internal class JenkinsArtifactInfo
+    {
+        public string FileName;
+        public string RelativePath;
+    }
+#pragma warning restore 649
+}
