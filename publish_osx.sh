205704df5ad2515519b0852c187d3c766be32cb7
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2020-10-23 15:13:35 +0200

    Attempt 3
    Create dirs for macOS.
    
    Git doesn't commit empty directories so the bin/ and dotnet/ folders that the macOS build script needs weren't in the repo.

--- publish_osx.sh
+++ publish_osx.sh
@@ -14,6 +14,10 @@ dotnet publish SS14.Launcher/SS14.Launcher.csproj -c Release --no-self-contained
 mkdir -p bin/publish/macOS
 
 cp -r "PublishFiles/Space Station 14 Launcher.app" bin/publish/macOS
+
+mkdir -p "bin/publish/macOS/Space Station 14 Launcher.app/Contents/Resources/dotnet/"
+mkdir -p "bin/publish/macOS/Space Station 14 Launcher.app/Contents/Resources/bin/"
+
 cp -r Dependencies/dotnet/mac/* "bin/publish/macOS/Space Station 14 Launcher.app/Contents/Resources/dotnet/"
 cp -r SS14.Launcher/bin/Release/netcoreapp3.1/osx-x64/publish/* "bin/publish/macOS/Space Station 14 Launcher.app/Contents/Resources/bin/"
 pushd bin/publish/macOS
