90e6eb25ea60457d98d3e70f44dcbc60ab4dccae
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2021-12-20 13:38:12 +0100

    Bump launcher version

--- SS14.Launcher/ConfigConstants.cs
+++ SS14.Launcher/ConfigConstants.cs
@@ -4,7 +4,7 @@ namespace SS14.Launcher;
 
 public static class ConfigConstants
 {
-    public const string CurrentLauncherVersion = "16";
+    public const string CurrentLauncherVersion = "17";
     public static readonly bool DoVersionCheck = true;
 
     // Refresh login tokens if they're within <this much> of expiry.
