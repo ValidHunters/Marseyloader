0f74bc799762563fb3a2592624ddfec54d911e05
Kara <lunarautomaton6@gmail.com>
2022-05-14 12:45:21 -0700

    Fix launcher version name (#64)

--- SS14.Launcher/LauncherVersion.cs
+++ SS14.Launcher/LauncherVersion.cs
@@ -4,6 +4,6 @@ namespace SS14.Launcher;
 
 public static class LauncherVersion
 {
-    public const string Name = "SS14.Laucher";
+    public const string Name = "SS14.Launcher";
     public static Version? Version => typeof(LauncherVersion).Assembly.GetName().Version;
 }
