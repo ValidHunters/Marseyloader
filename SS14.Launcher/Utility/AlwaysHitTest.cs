ad506ab71db9ac1d95ba224c8e57035a8a2de02e
Pieter-Jan Briers <pieterjan.briers@gmail.com>
2023-05-01 12:17:24 +0200

    Fix CheckBox and Expander gaps between icon and label.
    Dumb Avalonia thing I have to work around, I guess.

---
+++ SS14.Launcher/Utility/AlwaysHitTest.cs
@@ -0,0 +1,14 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Rendering;
+
+namespace SS14.Launcher.Utility;
+
+/// <summary>
+/// Utility control that always hit tests in its geometry region.
+/// Necessary to paper over some controls in Avalonia's default theme that have some annoying gaps otherwise.
+/// </summary>
+public sealed class AlwaysHitTest : Panel, ICustomHitTest
+{
+    public bool HitTest(Point point) => true;
+}
