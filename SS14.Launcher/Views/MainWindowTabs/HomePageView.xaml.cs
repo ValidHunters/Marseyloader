f26806515d2f8ba5a29b1dea09777621420773e5
Pieter-Jan Briers <pieterjan.briers@gmail.com>
2023-06-25 13:11:04 +0200

    Add a file dialog to open content bundles.
    Drag/drop isn't implemented by Avalonia on Linux, so this is the fallback.

--- SS14.Launcher/Views/MainWindowTabs/HomePageView.xaml.cs
+++ SS14.Launcher/Views/MainWindowTabs/HomePageView.xaml.cs
@@ -1,6 +1,10 @@
 using System;
+using System.Collections.Generic;
 using Avalonia.Controls;
-using Avalonia.Markup.Xaml;
+using Avalonia.Interactivity;
+using Avalonia.VisualTree;
+using Serilog;
+using SS14.Launcher.ViewModels;
 using SS14.Launcher.ViewModels.MainWindowTabs;
 
 namespace SS14.Launcher.Views.MainWindowTabs;
@@ -29,5 +33,39 @@ public partial class HomePageView : UserControl
         }
 
         base.OnDataContextChanged(e);
+    }
+
+    private async void OpenReplayClicked(object? sender, RoutedEventArgs e)
+    {
+        if (_viewModel?.MainWindowViewModel is not { } mainVm)
+            return;
+
+        if (this.GetVisualRoot() is not Window window)
+        {
+            Log.Error("Visual root isn't a window!");
+            return;
+        }
+
+        var dialog = new OpenFileDialog
+        {
+            Filters = new List<FileDialogFilter>
+            {
+                new() { Extensions = new List<string> { "zip" }, Name = "Content bundle files" }
+            },
+            Title = "Open replay or content bundle"
+        };
+
+        var result = await dialog.ShowAsync(window);
+        if (result == null)
+            return;
+
+        var file = result[0];
+        if (!mainVm.IsContentBundleDropValid(file))
+        {
+            // TODO: Report this nicely.
+            return;
+        }
+
+        ConnectingViewModel.StartContentBundle(mainVm, file);
     }
 }
