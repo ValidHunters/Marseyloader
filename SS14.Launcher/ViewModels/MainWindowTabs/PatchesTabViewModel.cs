e1a5c843a1948f86b6d4e9e056a754a77fd20ac8
Kaede <malchanceux@protonmail.com>
2023-11-24 18:53:40 +0200

    Moment

--- SS14.Launcher/ViewModels/MainWindowTabs/PatchesTabViewModel.cs
+++ SS14.Launcher/ViewModels/MainWindowTabs/PatchesTabViewModel.cs
@@ -63,11 +63,11 @@ public class PathToFileNameConverter : IValueConverter
     public object? Convert(object value, Type targetType, object parameter, CultureInfo culture)
     {
         var path = value as string;
-        return System.IO.Path.GetFileName(path);
+        return Path.GetFileName(path);
     }
 
     public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
-        throw new NotImplementedException();
+        return value;
     }
 }
