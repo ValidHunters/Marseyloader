ca80a91de054d89fbf1e6f986aa54b8ebdeed374
Kaede <malchanceux@protonmail.com>
2023-11-28 02:04:25 +0200

    Ground publish to dotnet version

--- publish_linux.sh
+++ publish_linux.sh
@@ -8,7 +8,7 @@ cd "$(dirname "$0")"
 rm -r **/bin bin/publish/Linux
 rm SS14.Launcher_Linux.zip
 
-dotnet publish SS14.Launcher/SS14.Launcher.csproj /p:FullRelease=True -c Release --no-self-contained -r linux-x64 /nologo /p:RobustILLink=true
+dotnet publish SS14.Launcher/SS14.Launcher.csproj /p:FullRelease=True -c Release --no-self-contained -r linux-x64 --framework net7.0 /nologo /p:RobustILLink=true
 dotnet publish SS14.Loader/SS14.Loader.csproj -c Release --no-self-contained -r linux-x64 /nologo
 
 # Create intermediate directories.
