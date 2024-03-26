c9d16341f9cce6720464318dde173fc7fc8f9f64
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2024-03-26 18:40:50 +0100

    Version: v0.26.1

--- SS14.Launcher/ConfigConstants.cs
+++ SS14.Launcher/ConfigConstants.cs
@@ -4,7 +4,7 @@ namespace SS14.Launcher;
 
 public static class ConfigConstants
 {
-    public const string CurrentLauncherVersion = "43";
+    public const string CurrentLauncherVersion = "44";
     public static readonly bool DoVersionCheck = true;
 
     // Refresh login tokens if they're within <this much> of expiry.
