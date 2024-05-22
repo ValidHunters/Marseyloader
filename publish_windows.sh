cb614cdc7d438bf38ad57985354aeeda57377753
Kaede <malchanceux@protonmail.com>
2024-05-22 12:10:08 +0300

    Reflect changes in the publish script

--- publish_windows.sh
+++ publish_windows.sh
@@ -23,8 +23,8 @@ mkdir -p bin/publish/Windows/Marsey/Mods
 mkdir -p bin/publish/Windows/Marsey/ResourcePacks
 
 cp -r Dependencies/dotnet/windows/* bin/publish/Windows/dotnet
-cp "SS14.Launcher.Bootstrap/bin/Release/net45/publish/Space Station 14 Launcher.exe" bin/publish/Windows
-cp "SS14.Launcher.Bootstrap/console.bat" bin/publish/Windows
+cp "SS14.Launcher.Strap/bin/Release/net45/publish/Marseyloader.exe" bin/publish/Windows
+cp "SS14.Launcher.Strap/console.bat" bin/publish/Windows
 cp SS14.Launcher/bin/Release/net8.0/win-x64/publish/* bin/publish/Windows/bin
 cp SS14.Loader/bin/Release/net8.0/win-x64/publish/* bin/publish/Windows/bin/loader
 
