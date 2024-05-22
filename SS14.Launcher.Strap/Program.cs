c44d40a6def3687f588a0f656c600288b72fee60
Kaede <malchanceux@protonmail.com>
2024-05-22 11:59:10 +0300

    omw kms

---
+++ SS14.Launcher.Strap/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace SS14.Launcher.Strap;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        string ourDir = GetAssemblyDirectory();
+        string dotnetDir = Path.Combine(ourDir, "dotnet");
+        string exeDir = Path.Combine(ourDir, "bin", "SS14.Launcher.exe");
+
+        SetEnvironmentVariable("DOTNET_ROOT", dotnetDir);
+        StartProcess(exeDir);
+    }
+
+    private static string GetAssemblyDirectory()
+    {
+        string path = typeof(Program).Assembly.Location;
+        return Path.GetDirectoryName(path);
+    }
+
+    private static void SetEnvironmentVariable(string variable, string value)
+    {
+        Environment.SetEnvironmentVariable(variable, value);
+    }
+
+    private static void StartProcess(string filePath)
+    {
+        Process.Start(new ProcessStartInfo(filePath));
+    }
+}
