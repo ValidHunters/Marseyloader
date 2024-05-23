ed3b7a69c7a010f9ea9414c5829ee2ff86fcf86e
Kaede <malchanceux@protonmail.com>
2024-05-23 18:06:56 +0300

    I'm actually unironically debating just axing the bootstrap

--- publish_windows.sh
+++ publish_windows.sh
@@ -10,7 +10,7 @@ rm SS14.Launcher_Windows.zip
 
 dotnet publish SS14.Launcher/SS14.Launcher.csproj /p:FullRelease=True -c Release --no-self-contained -r win-x64 /nologo /p:RobustILLink=true
 dotnet publish SS14.Loader/SS14.Loader.csproj -c Release --no-self-contained -r win-x64 /nologo
-dotnet publish SS14.Launcher.Bootstrap/SS14.Launcher.Bootstrap.csproj -c Release /nologo
+dotnet publish SS14.Launcher.Strap/SS14.Launcher.Strap.csproj -c Release /nologo
 
 ./exe_set_subsystem.py "SS14.Launcher/bin/Release/net8.0/win-x64/publish/SS14.Launcher.exe" 2
 ./exe_set_subsystem.py "SS14.Loader/bin/Release/net8.0/win-x64/publish/SS14.Loader.exe" 2
