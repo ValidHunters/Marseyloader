7186e61ec8f7ec7b24395900bce61c6663dbeb83
Kaede <malchanceux@protonmail.com>
2024-05-26 16:14:41 +0300

    Sedition - helper class to hide the assembly on request from the assembly

---
+++ Marsey/Subversion/Sedition.cs
@@ -0,0 +1,52 @@
+﻿using System.Reflection;
+using Marsey.Misc;
+using Marsey.Stealthsey;
+
+namespace Marsey.Subversion;
+
+/// <summary>
+///     Manages Hidesey patch helper class
+/// </summary>
+public static class Sedition
+{
+    private static void HideDelegate(Assembly asm)
+    {
+        MarseyLogger.Log(MarseyLogger.LogType.DEBG, "Hiding");
+        Hidesey.HidePatch(asm);
+    }
+
+    public static void InitHidesey(Assembly assembly, string? assemblyName)
+    {
+        Type? hideseyType = assembly.GetType("Hidesey");
+        if (hideseyType == null)
+        {
+            MarseyLogger.Log(MarseyLogger.LogType.DEBG, $"{assemblyName} has no Hidesey class");
+            return;
+        }
+
+        SetupHidesey(hideseyType);
+    }
+
+    private static void SetupHidesey(Type hidesey)
+    {
+        MethodInfo? stealthseyHide =
+            typeof(Sedition).GetMethod("HideDelegate", BindingFlags.Static | BindingFlags.NonPublic);
+        FieldInfo? asmHideseyDelegate = hidesey.GetField("hideDelegate", BindingFlags.Public | BindingFlags.Static);
+
+        if (asmHideseyDelegate == null || stealthseyHide == null)
+        {
+            MarseyLogger.Log(MarseyLogger.LogType.ERRO, $"Missing delegate fields on hidesey");
+            return;
+        }
+
+        try
+        {
+            Delegate logDelegate = Delegate.CreateDelegate(asmHideseyDelegate.FieldType, stealthseyHide);
+            asmHideseyDelegate.SetValue(null, logDelegate);
+        }
+        catch (Exception e)
+        {
+            MarseyLogger.Log(MarseyLogger.LogType.FATL, $"Failed to to assign logger delegate: {e.Message}");
+        }
+    }
+}
--- Marsey/Subversion/Subverse.cs
+++ Marsey/Subversion/Subverse.cs
@@ -61,6 +61,7 @@ public static class Subverse
             Assembly subverterAssembly = Assembly.LoadFrom(path);
             MarseyLogger.Log(MarseyLogger.LogType.DEBG, "Subversion", $"Sideloading {path}");
             AssemblyFieldHandler.InitLogger(subverterAssembly, subverterAssembly.FullName);
+            Sedition.InitHidesey(subverterAssembly, subverterAssembly.FullName);
 
             loadGameAssemblyMethod.Invoke(__instance, new object[] { subverterAssembly });
 
