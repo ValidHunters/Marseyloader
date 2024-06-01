bf652c9993cac7043eae08286f0934fe32d23f14
Conchelle <mary@thughunt.ing>
2024-06-01 22:47:25 +0300

    Bump version for headers

--- SS14.Launcher/ConfigConstants.cs
+++ SS14.Launcher/ConfigConstants.cs
@@ -5,7 +5,7 @@ namespace SS14.Launcher;
 
 public static class ConfigConstants
 {
-    public const string CurrentLauncherVersion = "47";
+    public const string CurrentLauncherVersion = "48";
     public static readonly bool DoVersionCheck = true;
 
     // Refresh login tokens if they're within <this much> of expiry.
